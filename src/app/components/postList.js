6b91235dc06d6bed634dec2cef078d5df1ce307b
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-11-30 17:51:24 +0200

    sort function

--- src/app/components/postList.js
+++ src/app/components/postList.js
@@ -11,51 +11,71 @@ export default class PostList extends React.Component {
       }
     }
     componentDidMount() {
-        this.listOfPosts()
+        this.loadListOfPosts()
       }
 
-    listOfPosts(){
-        new PostApi().getList().then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                posts: result
-              });
-            },
-        )
+    loadListOfPosts(){
+      new PostApi().getList().then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            posts: result
+          });
+        },
+      )
     }
 
     renderPostsList(){
-        const {error,  isLoaded, posts } = this.state;
-        if (error) {
-            return <div>Error: {error.message}</div>
-          } else if (!isLoaded) {
-            return <div>Loading...</div>
-          } else {
-            return(
-                <div id='posts'>
-                    { posts.map(post =>                
-                        <div key={post.id} className="post">
-                            <p><b> {post.title}</b> </p>
-                            <p>{post.body}</p>
-                        </div>           
-                )}
-            </div>
+      const {error,  isLoaded, posts } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>
+      } else if (!isLoaded) {
+        return <div>Loading...</div>
+      } else {
+        return(
+          <div id='posts'>
+            { posts.map(post =>                
+              <div key={post.id} className="post">
+                <p><b> {post.title}</b> </p>
+                <p>{post.body}</p>
+              </div>           
             )}
+          </div>
+          )}
     }
 
+  render() {
+    return (
+      <div> 
+        <div className='header'>
+          <div className="inl left">
+            <input type="text" className='beauty'/>
+          </div>
+          <div className='inl right'>
+            <select id='sort' className='beauty' onChange={(event)=>{this.sortPosts(event.target.value)}}>
+              <option>Default sort</option>
+              <option>Sort A-Z</option>
+              <option>Sort Z-A</option>
+            </select>
+          </div>
+        </div>
+        <div>
+            {this.renderPostsList()}
+        </div>
+      </div>  
+  )}  
 
-
-    render() {
-        return (
-          <div> 
-                <div className='header'>
-                    <div className="inl left">Filter</div>
-                    <div className='inl right'>Sort</div>
-                </div>
-                <div>
-                    {this.renderPostsList()}
-                </div>
-          </div>  
-        )}  
+  sortPosts(val){   
+    if(val === "Sort A-Z"){
+      const newSort = this.state.posts.sort((a,b)=>{ return a.title < b.title  ? -1 : 1 })
+      this.setState({posts: newSort })
+    }
+    else if (val === 'Sort Z-A' ){
+      const newSort = this.state.posts.sort((a,b)=>{ return a.title > b.title  ? -1 : 1 })
+      this.setState({posts: newSort })
+    }
+    else{
+      this.loadListOfPosts()
+    }  
+  }  
 }
--- src/app/services/postApi.js
+++ src/app/services/postApi.js
@@ -2,9 +2,9 @@
 export default class PostApi{
     
     getList(){
-        return (fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        )
+      return (fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      )
     }
 
 }
